0b8b874ae71374f3214a38abd20b6a6f9db68ade
lipengjie <158121260@qq.com>
2017-09-01 11:38:16 +0800

    添加标签功能

--- models/post.js
+++ models/post.js
@@ -7,9 +7,10 @@ var markdown = require('markdown').markdown;
 //name：发表文章的用户名
 //title：文章标题
 //post：文章内容
-function Post(name,title,post) {
+function Post(name,title,tags,post) {
     this.name = name;
     this.title = title;
+    this.tags = tags;
     this.post = post;
 }
 module.exports = Post;
@@ -42,6 +43,7 @@ Post.prototype.save = function (callback) {
         name:this.name,
         time:time,
         title:this.title,
+        tags:this.tags,
         post:this.post,
         //增加一个留言字段
         comments:[]
@@ -236,6 +238,56 @@ Post.getArchive = function (callback) {
                     return callback(err);
                 }
                 callback(null,docs);
+            })
+        })
+    })
+}
+// 返回所有的标签
+Post.getTags = function (callback) {
+    mongo.open(function (err,db) {
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function (err,collection) {
+            if(err){
+                mongo.close();
+                return callback(err);
+            }
+            collection.distinct('tags',function (err,tags) {
+                mongo.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null,tags)
+            })
+        })
+    })
+}
+//返回一个标签对应的所有文章
+Post.getTag = function (tag,callback) {
+    mongo.open(function (err,db) {
+        if(err){
+            return callback(err)
+        }
+        db.collection('posts',function (err,collection) {
+            if(err){
+                mongo.close();
+                return callback(err);
+            }
+            collection.find({
+                "tags":tag
+            },{
+                "name":1,
+                "time":1,
+                "title":1
+            }).sort({
+                time:-1
+            }).toArray(function (err, docs) {
+                mongo.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null,docs)
             })
         })
     })
